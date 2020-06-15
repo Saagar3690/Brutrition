f137790a40f808df5722cf9b3ca6f9c3cf3213cd
Saagar <saagar.mahadev123@gmail.com>
2020-06-15 14:43:30 -0700

    first commit

--- app.py
+++ app.py
@@ -24,3 +24,6 @@ def id():
 @app.errorhandler(404)
 def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+if __name__ == "__main__":
+  app.run(threaded=True, port=5000)
