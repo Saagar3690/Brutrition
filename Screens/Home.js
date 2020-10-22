baa4277374b33885f8a42666a1ea2bb5ab774a53
unknown <kirtanushah@gmail.com>
2020-10-21 18:28:55 -0700

    Nutrition Label

--- Screens/Home.js
+++ Screens/Home.js
@@ -31,24 +31,6 @@ render() {
         value={'Share with your friends...'}
         />
       */}
-      <NutritionLabel
-        servingSize={'1 cup (228g)'}
-        servingsPerContainer={2}
-        calories={260}
-        totalFat={13}
-        saturatedFat={5}
-        transFat={2}
-        cholesterol={30}
-        sodium={660}
-        totalCarbs={31}
-        dietaryFiber={0}
-        sugars={5}
-        protein={5}
-        vitaminA={4}
-        vitaminC={2}
-        calcium={15}
-        iron={4}
-      />
     </View>
   )
 }
--- Screens/Settings.js
+++ Screens/Settings.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
-import { Text, View } from 'react-native';
-import Autocomplete from 'react-native-autocomplete-input';
-
+import { Text, View, StyleSheet } from 'react-native';
+import NutritionLabel from '../Components/NutritionLabel'
 import TopBar from '../Components/TopBar'
 
 export default class Settings extends React.Component {
@@ -16,10 +15,47 @@ export default class Settings extends React.Component {
     return (
       <View>
         <TopBar/>
-        <View>
-          <Text>{this.state.title}!</Text>
+        <View style={styles.container}>
+          <NutritionLabel
+            style={styles.label}
+            servingSize={'1 cup (228g)'}
+            servingsPerContainer={2}
+            calories={260}
+            totalFat={13}
+            totalFatPercent={'20%'}
+            saturatedFat={5}
+            saturatedFatPercent={'10%'}
+            transFat={2}
+            cholesterol={30}
+            cholesterolPercent={'14%'}
+            sodium={660}
+            sodiumPercent={'21%'}
+            totalCarbs={31}
+            totalCarbsPercent={'11%'}
+            dietaryFiber={0}
+            dietaryFiberPercent={'0%'}
+            sugars={5}
+            protein={5}
+            vitaminA={'30%'}
+            vitaminC={'5%'}
+            calcium={'8%'}
+            iron={'45%'}
+        />
         </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row', 
+    justifyContent: 'center',
+    marginTop: 10
+  },
+  label: {
+    width: '70%',
+    borderColor: 'black',
+    borderWidth: 1
+  }
+})
